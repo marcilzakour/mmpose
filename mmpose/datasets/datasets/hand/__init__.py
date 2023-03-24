8c754177865875adeae267e033bd1f39639598c2
ge68boc <ge68boc@tum.de>
2023-03-24 13:27:12 +0100

    added coco_hands

--- mmpose/datasets/datasets/hand/__init__.py
+++ mmpose/datasets/datasets/hand/__init__.py
@@ -6,9 +6,9 @@ from .interhand3d_dataset import InterHand3DDataset
 from .onehand10k_dataset import OneHand10KDataset
 from .panoptic_hand2d_dataset import PanopticDataset
 from .rhd2d_dataset import Rhd2DDataset
-
+from .coco_hands import COCOHands
 __all__ = [
     'FreiHandDataset', 'InterHand2DDataset', 'InterHand3DDataset',
     'OneHand10KDataset', 'PanopticDataset', 'Rhd2DDataset',
-    'HandCocoWholeBodyDataset'
+    'HandCocoWholeBodyDataset', 'COCOHands'
 ]
